ca411f6843e1c5a1cd12a698853aba69f4566e76
thuongtruong1009 <ititiu19228@student.hcmiu.edu.vn>
2021-12-11 12:24:18 +0700

    initial  group step 1

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="grid">
+    <div class="group-header flex justify-between items-center">
+      <h4>Social</h4>
+      <div class="extension flex">
+        <img src="/public/assets/images/aside-icons/add-friend.png">
+        <img src="/public/assets/images/aside-icons/add-folder.png">
+        <img src="/public/assets/images/aside-icons/options.png">
+        <img src="/public/assets/images/aside-icons/search.png">
+      </div>
+    </div>
+  </div>
+</template>
--- src/layouts/client.vue
+++ src/layouts/client.vue
@@ -1,8 +1,9 @@
 <template>
   <main class="text-center text-gray-700 dark:text-gray-200 h-screen">
     <router-view />
-    <div class="mt-5 mx-auto text-center opacity-25 text-sm">
+    <div class="flex justify-between">
       [Client Home Overview Layout]
+      <Sidebar />
     </div>
   </main>
 </template>
